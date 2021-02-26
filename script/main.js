64f57d43d27025f4e5704d2ebf554312a8e60e0b
thijnja <thijnjacobs11@gmail.com>
2021-02-26 10:40:53 +0100

    Doughnut chartset toegevoegd voor mobiel

--- script/main.js
+++ script/main.js
@@ -8,8 +8,8 @@ var chart = new Chart(ctx, {
         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
         datasets: [{
             label: 'My First dataset',
-            backgroundColor: '  #000000',
-            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgb(255, 255, 255)',
+            borderColor: 'rgb(255, 255, 255)',
             data: [0, 10, 5, 2, 20, 30, 45]
         }]
     },
@@ -17,3 +17,62 @@ var chart = new Chart(ctx, {
     // Configuration options go here
     options: {}
 });
+
+/* document.getElementById('doughnut');
+var myDoughnutChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['orange', 'red'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor:[
+            window.chartColors.orange,
+            window.chartColors.red ],
+            borderColor: 'rgb(255, 255, 255)',
+            data: [0, 10]
+        }]
+    },
+    options: {}
+});
+*/
+
+
+  
+
+    var ctz = document.getElementById('doughnut')
+
+        var myDoughnut = new Chart(ctz, {
+            type: 'doughnut',
+            data: {
+                datasets: [{
+                    data: [
+                        10, 90
+                    ],
+                    backgroundColor: 'rgb(58, 255, 36)',
+                    
+                    label: 'Dataset 1'
+                }],
+                labels: [
+                    'Red',
+                    'Orange',
+                ]
+            },
+            options: {
+                responsive: true,
+                legend: {
+                    position: 'top',
+                },
+                title: {
+                    display: true,
+                    text: 'Vloeistoffen aanwezig'
+                },
+                animation: {
+                    animateScale: true,
+                    animateRotate: true
+                }
+            }
+        });
+
+        
+
+      
