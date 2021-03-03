3f8f15462f3fbbabbb2baba733ae1973466be715
thijnja <thijnjacobs11@gmail.com>
2021-03-03 17:09:28 +0100

    Comments js en extra stijling css

--- script/main.js
+++ script/main.js
@@ -1,26 +1,11 @@
+// Ik heb voor de eerste drie datavisualisaties chart.js gebruikt en de laatste heb ik zelf gemaakt. 
+
+
 Chart.defaults.global.defaultFontFamily = "'Arial';";
 Chart.defaults.global.defaultFontColor = 'black';
 
-/* document.getElementById('doughnut');
-var myDoughnutChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        labels: ['orange', 'red'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor:[
-            window.chartColors.orange,
-            window.chartColors.red ],
-            borderColor: 'rgb(255, 255, 255)',
-            data: [0, 10]
-        }]
-    },
-    options: {}
-});
-*/
 
-
-// Ik heb voor de datavisualisatie chart.js gebruikt. //  
+// Javascript voor de brandstof meter.  
 
     var ctz = document.getElementById('doughnut')
 
@@ -59,6 +44,7 @@ var myDoughnutChart = new Chart(ctx, {
             }
         });
 
+// Javascript voor de elektriciteit meter. 
 var ctz = document.getElementById('doughnut2')
 
         var myDoughnut = new Chart(ctz, {
@@ -98,13 +84,13 @@ var ctz = document.getElementById('doughnut2')
 
 
 
-
+// Javascript voor de afgelegde kilometers. 
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
-    // The type of chart we want to create
+    // Type datvisualisatie
     type: 'line',
 
-    // The data for our dataset
+    // De data van de grafiek
     data: {
         labels: ['0 uur', '2 uur', '4 uur', '6 uur', '8 uur', '10 uur', '12 uur'],
         datasets: [{
@@ -114,9 +100,6 @@ var chart = new Chart(ctx, {
             data: [3000, 2080, 1500, 1400, 1400, 3000, 4500]
         }]
     },
-
-    // Configuration options go here
-    options: {}
 });    
 
       
